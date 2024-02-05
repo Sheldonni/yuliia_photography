be4f13519042ee975bd4b0492ba4c50a7288a2d4
Sheldonni <149395414+Sheldonni@users.noreply.github.com>
2024-02-05 19:37:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// script.js
+
+function zoomPhoto(element) {
+    var zoomedImage = document.getElementById('zoomedImage');
+    var zoomedContainer = document.getElementById('zoomedContainer');
+
+    if (element.classList.contains('zoomed')) {
+        // Якщо клікнуто на вже збільшене зображення, приховуємо його
+        element.classList.remove('zoomed');
+        zoomedContainer.style.display = 'none';
+    } else {
+        // Інакше визначаємо нове зображення та показуємо його
+        element.classList.add('zoomed');
+        zoomedImage.src = element.src;
+        zoomedImage.alt = element.alt;
+        zoomedContainer.style.display = 'flex';
+    }
+}
+
+function closeZoomed() {
+    var zoomedImage = document.getElementById('zoomedImage');
+    var zoomedContainer = document.getElementById('zoomedContainer');
+
+    zoomedImage.src = ''; // Очищаємо зображення
+    zoomedImage.alt = '';
+    zoomedContainer.style.display = 'none'; // Приховуємо контейнер
+}
+
+
+
+const send = document.getElementById('myButton');
+
+send.addEventListener('click', () => {
+    send.classList.add('clicked');
+    setTimeout(() => {
+        send.classList.remove('clicked');
+    }, 500);
+});
+
+
+
+
+const reset = document.getElementById('myButton');
+
+send.addEventListener('click', () => {
+    send.classList.add('clicked');
+    setTimeout(() => {
+        send.classList.remove('clicked');
+    }, 500);
+});
+
+
+
+
